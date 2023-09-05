f787f26a638ec899104aae37d460de998838a430
GitXBot101 <117470125+GitXBot101@users.noreply.github.com>
2023-09-05 20:13:39 +0530

    Update localisation.py

--- bot/localisation.py
+++ bot/localisation.py
@@ -6,7 +6,7 @@ from bot.get_cfg import get_config
 
 
 class Localisation:
-    START_TEXT = "Hello, \n\nThis is a Telegram <b>Video Encoder Bot</b>. \n\n<b>Please send me any Telegram big video file I will compress it as s small video file!</b> \n\n/help for more details. \n\n🔥 Join : https://t.me/+2DSmixSyvzU4ZGUy"
+    START_TEXT = "Hello, \n\nThis is a Telegram <b>Video Encoder Bot</b>. \n\n<b>Please send me any Telegram big video file I will compress it as s small video file!</b> \n\n/help for more details. \n\n✨ Enjoy....."
    
     ABS_TEXT = " Please don't be selfish."
     
