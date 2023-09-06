1f214ea97b44b1625e602bfffafee89bf8cd44c7
GitXBot101 <117470125+GitXBot101@users.noreply.github.com>
2023-09-06 08:33:12 +0530

    Update utils.py

--- bot/helper_funcs/utils.py
+++ bot/helper_funcs/utils.py
@@ -24,6 +24,17 @@ def checkKey(dict, key):
   else:
     return False
 
+def hbs(size):
+    if not size:
+        return ""
+    power = 2 ** 10
+    raised_to_pow = 0
+    dict_power_n = {0: "B", 1: "K", 2: "M", 3: "G", 4: "T", 5: "P"}
+    while size > power:
+        size /= power
+        raised_to_pow += 1
+    return str(round(size, 2)) + " " + dict_power_n[raised_to_pow] + "B"
+
 async def on_task_complete():
     del data[0]
     if len(data) > 0:
